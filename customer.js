535af67bbe6e28298d1ce4ab08b1a944f697fc15
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-10 11:12:10 -0400

    add initial structure to customer.js

--- customer.js
+++ customer.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
+const pg = require('pg-promise')();
+const dbConfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME
 
-// var db = require('db')
-// db.connect({
-//     host: process.eventNames.DB_HOST,
-//     username: process.env.DB_USER
-// });
+}
+const db = pg(dbConfig);
+
+class Customer {
+    constructor() {
+
+    }
+    save() {
+        
+    }
+}
+
+module.exports = Customer;
